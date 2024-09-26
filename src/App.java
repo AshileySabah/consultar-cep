bbaf9a4bbbc050fb8277d95b77ca4a471380a2d8
AshileySabah <ashileysabrina2002@gmail.com>
2024-09-26 18:58:07 -0300

    refact: App

--- src/App.java
+++ src/App.java
@@ -1,10 +1,3 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -15,13 +8,10 @@ import com.google.gson.GsonBuilder;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        BufferedWriter writer;
 
         Gson gson = new GsonBuilder()
         .setPrettyPrinting()
         .create();
-
-        String cep = "";
 
         List<Endereco> enderecos = new ArrayList<Endereco>();
 
@@ -35,19 +25,9 @@ public class App {
 
             if (continuar == 1) {
                 System.out.println("Digite o cep: ");
-                cep = scanner.nextLine();
-        
-                String url = "https://viacep.com.br/ws/" + cep.replace("-", "") + "/json/";
-        
-                HttpClient client = HttpClient.newHttpClient();
-        
-                HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .build();
-        
-                HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+                String cep = scanner.nextLine();
 
-                String json = response.body();
+                String json = new EnderecoService().consultarEnderecoPorCEP(cep);
         
                 EnderecoRecord enderecoRecord = gson.fromJson(json, EnderecoRecord.class);
                 Endereco endereco = new Endereco(enderecoRecord);
@@ -55,16 +35,9 @@ public class App {
             }
         }
 
-        writer = new BufferedWriter(new FileWriter("lista-enderecos.txt"));
-        for (Endereco endereco : enderecos) {
-            writer.write(endereco.toString());
-            writer.newLine();
-        }
-        writer.close();
+        new GerenciadorArquivo().gerarArquivoTXT(enderecos);
 
-        writer = new BufferedWriter(new FileWriter("lista-enderecos.json"));
-        writer.write(gson.toJson(enderecos));
-        writer.close();
+        new GerenciadorArquivo().gerarArquivoJSON(enderecos);
 
         System.out.println(enderecos);
 
