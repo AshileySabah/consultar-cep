02e735394b417da8ca8c56d6945a441f8971d28f
AshileySabah <ashileysabrina2002@gmail.com>
2024-09-26 19:13:34 -0300

    refact: App

--- src/App.java
+++ src/App.java
@@ -1,46 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-
-        Gson gson = new GsonBuilder()
-        .setPrettyPrinting()
-        .create();
-
         List<Endereco> enderecos = new ArrayList<Endereco>();
+        boolean continuar = true;
 
-        int continuar = 1;
+        while (continuar) {
+            continuar = new Console().mostrarMenu();
 
-        while (continuar == 1) {
-            System.out.println("0 - Sair");
-            System.out.println("1 - Continuar");
-            continuar = scanner.nextInt();
-            scanner.nextLine();
-
-            if (continuar == 1) {
-                System.out.println("Digite o cep: ");
-                String cep = scanner.nextLine();
-
-                String json = new EnderecoService().consultarEnderecoPorCEP(cep);
-        
-                EnderecoRecord enderecoRecord = gson.fromJson(json, EnderecoRecord.class);
-                Endereco endereco = new Endereco(enderecoRecord);
+            if (continuar) {
+                String cep = new Console().solicitarCep();
+                Endereco endereco = new EnderecoService().consultarEnderecoPorCEP(cep);
                 enderecos.add(endereco);
             }
         }
 
         new GerenciadorArquivo().gerarArquivoTXT(enderecos);
-
         new GerenciadorArquivo().gerarArquivoJSON(enderecos);
-
         System.out.println(enderecos);
-
-        scanner.close();
     }
 }
